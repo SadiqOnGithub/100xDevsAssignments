26fd538330fec3076d5513837f7943289fad3c02
SadiqOnGithub <sadiqonemail@gmail.com>
2024-03-29 10:54:00 +0530

    passing 4 test; 1 left

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -3,10 +3,19 @@
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
+const inclu = (arr1, arr2) => arr1.every(item => arr2.includes(item));
 
 function isAnagram(str1, str2) {
+  const arr1 = str1.split(''),
+    arr2 = str2.split('');
 
-  return str1 === str2.split('').reverse();
+  if (arr1.length !== arr2.length) return false;
+
+  const returnVal = inclu(arr1, arr2) && inclu(arr2, arr1) ? true : false;
+
+  console.log(returnVal);
+
+  return returnVal
 }
 
 module.exports = isAnagram;
