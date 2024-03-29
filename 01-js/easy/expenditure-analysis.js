cbba5d2afde2d35c9b7726cc8e8d7f33c8fb4f81
SadiqOnGithub <sadiqonemail@gmail.com>
2024-03-29 11:54:03 +0530

    my complicated solution of easy/expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -4,17 +4,30 @@
   transactions is an array where each
   Transaction - an object like 
         {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  }
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  var returnArr = [];
+  transactions.map(ele => ({
+    category: ele.category,
+    totalSpent: ele.price
+  })).forEach(element => {
+    (returnArr.some(item => item.category == element.category))
+      ? (
+        returnArr = returnArr.map(item => item.category !== element.category ? item : ({
+          category: item.category,
+          totalSpent: item.totalSpent + element.totalSpent
+        })))
+      : returnArr.push(element);
+  });
+  return returnArr;
 }
 
 module.exports = calculateTotalSpentByCategory;
