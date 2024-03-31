d5327dfd102185c80eac3209724d840ae35f6da7
SadiqOnGithub <sadiqonemail@gmail.com>
2024-04-01 04:19:18 +0530

    week-3/mongo/schema added

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,25 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb://localhost:8027/');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  username: String,
+  password: String
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  username: String,
+  password: String
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  title: String,
+  description: String,
+  price: Number,
+  imageLink: String,
+  published: Boolean
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
